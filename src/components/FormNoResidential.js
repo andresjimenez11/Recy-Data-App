e1d07a69156fab1b7479e3cf850e3d5fd1445d0b
Andrés Felipe Jiménez Sánchez <njimenezgiraldo@gmail.com>
2024-10-30 18:16:12 -0500

    feat: components form

---
+++ src/components/FormNoResidential.js
@@ -0,0 +1,220 @@
+import React, { useState } from 'react';
+import { View, TextInput, Text } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
+import formStyles from '../styles/formStyles';
+
+import colors from '../themes/colors';
+
+export default function FormNoResidential() {
+
+  const [selectedSubType, setSelectedSubType] = useState('Industrial');
+  const [selectedCommune, setSelectedCommune] = useState('Comuna 1');
+  const [selectedCity, setSelectedCity] = useState('Bucaramanga');
+
+
+  return (
+
+        <View>
+            <View style={formStyles.containerForm}>
+                <Text style={formStyles.labelRightSelector}>Sub Tipo</Text>
+                <View style={formStyles.pickerContainer}>
+                <Picker
+                    style={formStyles.pickerRight}
+                    selectedValue={selectedSubType}
+                    onValueChange={(itemValue) => setSelectedSubType(itemValue)}
+                >
+                    <Picker.Item label="Industrial" value="Industrial" />
+                    <Picker.Item label="Comercial" value="Comercial" />
+                    <Picker.Item label="Institucional" value="Institucional" />
+
+                </Picker>
+                </View>
+            </View> 
+
+            <View style={formStyles.containerForm}>
+                <TextInput
+                    style={formStyles.input}
+                    placeholder="Codigo del recibo de aseo"
+                    placeholderTextColor={colors.primary}
+                />
+            </View>
+
+            <View style={formStyles.containerForm}>
+                <TextInput
+                    style={formStyles.input}
+                    placeholder="Nombre"
+                    placeholderTextColor={colors.primary}
+                />
+            </View>
+                
+            <View style={formStyles.containerForm}>
+                <TextInput
+                    style={formStyles.input}
+                    placeholder="Apellido"
+                    placeholderTextColor={colors.primary}
+                />
+            </View>
+
+            <View style={formStyles.containerForm}>
+                <TextInput
+                    style={[formStyles.input, formStyles.inputAddress]}
+                    placeholder="Autopista"
+                    placeholderTextColor={colors.primary}
+                />
+                <TextInput
+                    style={[formStyles.input, formStyles.inputAddressNumber]}
+                    placeholder="00"
+                    placeholderTextColor={colors.primary}
+                />
+                <Text
+                style={formStyles.labelAddressNumber}>
+                    #
+                </Text>
+                <TextInput
+                    style={[formStyles.input, formStyles.inputAddressNumber]}
+                    placeholder="00"
+                    placeholderTextColor={colors.primary}
+                />
+                <Text
+                style={formStyles.labelAddressNumber}>
+                    -
+                </Text>
+                <TextInput
+                    style={[formStyles.input, formStyles.inputAddressNumber]}
+                    placeholder="00"
+                    placeholderTextColor={colors.primary}
+                />
+            </View>
+                
+                
+            <View style={formStyles.containerForm}>
+                <View style={formStyles.pickerContainer2}>
+                    <Picker
+                        style={formStyles.pickerRight}
+                        selectedValue={selectedCommune}
+                        onValueChange={(itemValue) => setSelectedCommune(itemValue)}
+                    >
+                        <Picker.Item label="Comuna 1" value="Comuna 1" />
+                        <Picker.Item label="Comuna 2" value="Comuna 2" />
+                        <Picker.Item label="Comuna 3" value="Comuna 3" />
+                    </Picker>
+                </View>
+            </View> 
+                
+
+            <View style={formStyles.containerForm}>
+                <View style={formStyles.pickerContainer2}>
+                    <Picker
+                        style={formStyles.pickerRight}
+                        selectedValue={selectedCity}
+                        onValueChange={(itemValue) => setSelectedCity(itemValue)}
+                    >
+                        <Picker.Item label="Bucaramanga" value="Bucaramanga" />
+                        <Picker.Item label="Barrancabermeja" value="Barrancabermeja" />
+
+                    </Picker>
+                </View>
+            </View> 
+
+            <View style={formStyles.containerForm}>
+                <TextInput
+                    style={formStyles.input}
+                    placeholder="Teléfono"
+                    placeholderTextColor={colors.primary}
+                />
+            </View>
+
+            <View style={formStyles.containerForm}>
+                <TextInput
+                    style={formStyles.input}
+                    placeholder="Email"
+                    placeholderTextColor={colors.primary}
+                />
+            </View>
+
+            <View style={formStyles.containerForm}>
+                <TextInput
+                    style={formStyles.input}
+                    placeholder="No. Personas generadoras de residuos"
+                    placeholderTextColor={colors.primary}
+                />
+            </View>
+
+            <View style={formStyles.containerForm}>
+                <TextInput
+                    style={formStyles.input}
+                    placeholder="Contraseña"
+                    placeholderTextColor={colors.primary}
+                />
+            </View>
+
+            <View style={formStyles.containerBottomForm}> 
+                <View style={formStyles.containerForm}>
+                    <Text style={formStyles.textContainerBottomForm}>Info Institucional</Text>
+                </View>
+                
+                <View style={formStyles.containerForm}>
+                    <Text style={[formStyles.labelRightSelector, formStyles.labelRightSelectorBottomForm]}>Tipo</Text>
+                    <View style={formStyles.pickerContainer}>
+                    <Picker
+                    style={formStyles.pickerRight}
+                    selectedValue={selectedCity}
+                    onValueChange={(itemValue) => setSelectedType(itemValue)}
+                    >
+                    <Picker.Item label="Residencial" value="Residencial" />
+                    <Picker.Item label="Comercial" value="Comercial" />
+                    <Picker.Item label="Industrial" value="Industrial" />
+                    </Picker>
+                    </View>
+                </View> 
+
+                <View style={formStyles.containerForm}>
+                    <Text style={[formStyles.labelRightSelector, formStyles.labelRightSelectorBottomForm]}>Nombre</Text>
+                    <View style={formStyles.pickerContainer}>
+                    <Picker
+                    style={formStyles.pickerRight}
+                    selectedValue={selectedCity}
+                    onValueChange={(itemValue) => setSelectedType(itemValue)}
+                    >
+                    <Picker.Item label="Residencial" value="Residencial" />
+                    <Picker.Item label="Comercial" value="Comercial" />
+                    <Picker.Item label="Industrial" value="Industrial" />
+                    </Picker>
+                    </View>
+                </View> 
+
+                <View style={formStyles.containerForm}>
+                    <Text style={[formStyles.labelRightSelector, formStyles.labelRightSelectorBottomForm]}>Grado</Text>
+                    <View style={formStyles.pickerContainer}>
+                    <Picker
+                    style={formStyles.pickerRight}
+                    selectedValue={selectedCity}
+                    onValueChange={(itemValue) => setSelectedType(itemValue)}
+                    >
+                    <Picker.Item label="Residencial" value="Residencial" />
+                    <Picker.Item label="Comercial" value="Comercial" />
+                    <Picker.Item label="Industrial" value="Industrial" />
+                    </Picker>
+                    </View>
+                </View> 
+
+                <View style={formStyles.containerForm}>
+                    <Text style={[formStyles.labelRightSelector, formStyles.labelRightSelectorBottomForm]}>Curso</Text>
+                    <View style={formStyles.pickerContainer}>
+                    <Picker
+                    style={formStyles.pickerRight}
+                    selectedValue={selectedCity}
+                    onValueChange={(itemValue) => setSelectedType(itemValue)}
+                    >
+                    <Picker.Item label="Residencial" value="Residencial" />
+                    <Picker.Item label="Comercial" value="Comercial" />
+                    <Picker.Item label="Industrial" value="Industrial" />
+                    </Picker>
+                    </View>
+                </View> 
+            </View>
+        </View>
+
+    );
+}
---
+++ src/styles/formStyles.js
@@ -0,0 +1,88 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+// Obtenemos las dimensiones de la pantalla
+const { width, height } = Dimensions.get('window');
+
+const formStyles = StyleSheet.create({
+
+    containerForm: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginHorizontal: 40,
+      },
+      input: {
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        color: '#176b00',
+        width: '100%',
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 10,
+        fontSize: 16,
+        fontFamily: 'Comfortaa'
+      },
+      inputAddress: {
+        width: '40%',
+        marginRight: 10,
+      },
+      inputAddressNumber:{
+        width: '15%',
+        textAlign: 'center',
+      },
+      labelAddressNumber: {
+        color: '#ffffff',
+      },
+      labelRightSelector: {
+        width: '30%',
+        color: '#ffffff',
+        fontFamily: 'Comfortaa',
+        fontSize: 16,
+      },
+  
+      // Contenedor formulario inferior
+      containerBottomForm: {
+        flex: 1,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        borderRadius: 50,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        marginTop: 20,
+        paddingTop: 15,
+        paddingBottom: 50,
+        alignItems: 'center',
+      },
+      textContainerBottomForm:
+      {
+        color: "#000000",
+        fontSize: 20,
+        fontFamily: 'Comfortaa',
+        marginBottom: 10, 
+      },
+      labelRightSelectorBottomForm:{
+        color: '#000000',
+      },
+      pickerContainer: {
+        width: '70%',
+        height: 50,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 10,
+        marginVertical: 5,
+        overflow: 'hidden',  // Asegura que el contenido se ajuste al borde redondeado
+      },
+      pickerContainer2: {
+        width: '100%',
+        height: 50,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 10,
+        marginVertical: 5,
+        overflow: 'hidden',  // Asegura que el contenido se ajuste al borde redondeado
+      },
+      pickerRight: {
+        color: '#176b00',
+        padding: 10,
+        fontFamily: 'Comfortaa',
+      }, 
+})
+
+export default formStyles;
