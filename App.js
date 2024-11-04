a92e7f0abee1aac39f199a09571eeaabd0842461
Andrés Felipe Jiménez Sánchez <njimenezgiraldo@gmail.com>
2024-11-04 18:52:30 -0500

    fix navigation, adding buttonlogin.js parameters

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,13 +7,12 @@ import Register from './src/screens/Register';
 import Login from './src/screens/Login';
 import ResetPassword from './src/screens/ResetPassword';
 import Home from './src/screens/Home';
-import RecyclingRecords from './src/screens/RecyclingRecords'
+import RecyclingRecords from './src/screens/RecyclingRecords';
+import RecyclingRecordsList from './src/screens/RecyclingRecordsList';
+import MainMenu from './src/screens/MainMenu';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { StyleSheet, Text } from 'react-native';
-import MainMenu from './src/screens/MainMenu';
-import RecyclingRecordsList from './src/screens/RecyclingRecordsList';
-
 
 SplashScreen.preventAutoHideAsync();
 
@@ -33,59 +32,79 @@ export default function App() {
     return null;
   }
 
-  const Stack = createStackNavigator();
+  const AuthStack = createStackNavigator();
+  const AppStack = createStackNavigator();
+  const RootStack = createStackNavigator();
 
-  function MyStack() {
+  // Stack para autenticación
+  function AuthNavigator() {
     return (
-      <Stack.Navigator screenOptions={{
-        headerTransparent: true,
-        headerTintColor: '#fff', // Íconos blancos para mayor contraste
-        headerStyle: {
-          backgroundColor: 'transparent',
-          elevation: 0,
-          shadowOpacity: 0,
-        },
-        headerTitleStyle: {
-          fontFamily: 'Comfortaa', // Cambia por tu fuente
-          fontSize: 22,
-          fontWeight: 'normal',
-          color: '#ffffff',
-        },
-        headerTitleAlign: 'center', // Alinea el título al centro
-      }}>
-        <Stack.Screen name="Main" component={Main} options={{ headerShown: false, title: 'Main' }}/>
-        <Stack.Screen name="Register" component={Register} options={{ headerShown: true, title: 'Registro'}}/>
-        <Stack.Screen name="Login" component={Login} options={{ title: 'Inicio de sesión' }}/>
-        <Stack.Screen name="ResetPassword"
-          component={ResetPassword}
-          options={{
-            headerShown: true,
-            headerTitle: () => (
-              <Text style={styles.headerTitle}>Recuperar{'\n'}contraseña</Text>
-            ),
-          }}
-        />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="MainMenu" component={MainMenu}options={{title: 'Menu principal'}}/>
-        <Stack.Screen name="RecyclingRecords" component={RecyclingRecords}options={{title: 'Registro de reciclaje'}}/>
-        <Stack.Screen name="RecyclingRecordsList" component={RecyclingRecordsList}options={{title: "Lista de Registros de Reciclaje"}}/>
-      </Stack.Navigator>
+      <AuthStack.Navigator screenOptions={stackScreenOptions}>
+        <AuthStack.Screen name="Main" component={Main} options={{ headerShown: false, title: 'Main' }} />
+        <AuthStack.Screen name="Register" component={Register} options={{ title: 'Registro' }} />
+        <AuthStack.Screen name="Login" component={Login} options={{ title: 'Inicio de sesión' }} />
+        <AuthStack.Screen name="ResetPassword" component={ResetPassword} options={{
+          headerShown: true,
+          headerTitle: () => (
+            <Text style={styles.headerTitle}>Recuperar{'\n'}contraseña</Text>
+          ),
+        }} />
+      </AuthStack.Navigator>
     );
   }
-  
+
+  // Stack para la aplicación después de la autenticación
+  function AppNavigator() {
+    return (
+      <AppStack.Navigator screenOptions={stackScreenOptions}>
+        <AppStack.Screen name="Home" component={Home} />
+        <AppStack.Screen name="MainMenu" component={MainMenu} options={{ title: 'Menu principal' }} />
+        <AppStack.Screen name="RecyclingRecords" component={RecyclingRecords} options={{ title: 'Registro de reciclaje' }} />
+        <AppStack.Screen name="RecyclingRecordsList" component={RecyclingRecordsList} options={{ title: "Lista de Registros de Reciclaje" }} />
+      </AppStack.Navigator>
+    );
+  }
+
+  // Navegador principal que combina ambos stacks
+  function RootNavigator() {
+    return (
+        <RootStack.Navigator screenOptions={{ headerShown: false }}>
+            <RootStack.Screen name="Auth" component={AuthNavigator} />
+            <RootStack.Screen name="App" component={AppNavigator} />
+        </RootStack.Navigator>
+    );
+}
+
   return (
     <NavigationContainer>
-      <MyStack/>
+        <RootNavigator />
     </NavigationContainer>
   );
 }
 
+const stackScreenOptions = {
+  headerTransparent: true,
+  headerTintColor: '#fff',
+  headerStyle: {
+    backgroundColor: 'transparent',
+    elevation: 0,
+    shadowOpacity: 0,
+  },
+  headerTitleStyle: {
+    fontFamily: 'Comfortaa',
+    fontSize: 22,
+    fontWeight: 'normal',
+    color: '#ffffff',
+  },
+  headerTitleAlign: 'center',
+};
+
 const styles = StyleSheet.create({
   headerTitle: {
     flex: 1,
-    fontSize: 22, // Ajusta el tamaño según sea necesario
-    flexWrap: 'wrap', // Permite que el texto se ajuste a múltiples líneas
-    textAlign: 'center', // Centra el texto
+    fontSize: 22,
+    flexWrap: 'wrap',
+    textAlign: 'center',
     fontFamily: 'Comfortaa',
     color: '#ffffff',
   },
