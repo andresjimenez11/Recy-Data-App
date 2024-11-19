8c07a8292d2ce964fee00ad9ff1e8164ce411f46
Andrés Felipe Jiménez Sánchez <njimenezgiraldo@gmail.com>
2024-11-19 18:10:38 -0500

    fix navigation main to login

---
+++ src/components/ButtonLgMain.js
@@ -0,0 +1,34 @@
+import { Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import strings from '../util/strings';
+
+const { width } = Dimensions.get('window');
+const baseWidth = 375;  // Ancho base de referencia (por ejemplo, iPhone X)
+const scale = width / baseWidth;
+
+export default function ButtonLgMain() {
+
+    const navigation = useNavigation();
+
+    return (
+        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.loginButtonText}>{strings.login}</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    loginButton: {
+        width: '50%',
+        paddingVertical: '5%',
+        marginBottom: 35,
+        borderRadius: 15,
+        backgroundColor: '#f7e650',
+        alignItems: 'center',
+    },
+    loginButtonText: {
+        fontSize: 18,
+        color: '#000000',
+        fontFamily: 'Comfortaa',
+    },
+  })
